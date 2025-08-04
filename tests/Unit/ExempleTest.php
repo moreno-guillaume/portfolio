e7744a41513e734552b05870db1cb0bdf3e643b6
moreno-guillaume <moreno-guillaume@outlook.fr>
2025-08-04 13:22:34 +0200

    Rectification grammaticale + phpcbf

--- tests/Unit/ExempleTest.php
+++ tests/Unit/ExempleTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class ExempleTest extends TestCase
 {
     public function testExample(): void
     {
