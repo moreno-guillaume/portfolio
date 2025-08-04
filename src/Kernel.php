00fb54d95e221e5a84f9154c934932fbbfcd478c
moreno-guillaume <moreno-guillaume@outlook.fr>
2025-08-04 15:53:47 +0200

    code sale, vérification des securites

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,6 +6,5 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
-{
-    use MicroKernelTrait;
+use MicroKernelTrait  ;
 }
